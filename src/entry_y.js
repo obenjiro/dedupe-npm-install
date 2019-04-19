8c773d78c94f53f6078cb504207a2c9e99756d31
Aleksey Okhrimenko <ayuokhrimenko@avito.ru>
2019-04-19 12:25:53 +0300

    initial

---
+++ src/entry_y.js
@@ -0,0 +1,6 @@
+import {a} from 'lib_a';
+import {b} from 'lib_b';
+
+import * as _ from 'lodash';
+
+console.log(a, b, _);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+  const TerserPlugin = require('terser-webpack-plugin');
+
+
+module.exports = {
+  mode: "production",
+  entry: {
+    x: "./src/entry_x.js",
+    y: "./src/entry_y.js"
+  },
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+        maxInitialRequests: 30,
+        maxAsyncRequests: 30,
+        chunks: 'all',
+        cacheGroups: {
+            vendors: {
+                test: /\/node_modules\/.+\.(js|es6)$/,
+                priority: -10
+            },
+            default: {
+                test: /\.(js|es6)$/,
+                minChunks: 1,
+                priority: -20,
+                reuseExistingChunk: true
+            }
+        }
+    }
+  },
+  plugins: [new BundleAnalyzerPlugin()]
+};
