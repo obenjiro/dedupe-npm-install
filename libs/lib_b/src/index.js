3fc168c744a7d021e922bd11f31607bcea0ce6c2
Aleksey Okhrimenko <ayuokhrimenko@avito.ru>
2019-04-29 00:44:25 +0300

    small refactoring

--- libs/lib_b/src/index.js
+++ libs/lib_b/src/index.js
@@ -1,3 +1,5 @@
-import * as _ from 'lodash';
+import * as _ from 'sigmund';
+
+console.log(_);
 
 export const b = _;
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
 const path = require('path')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-    .BundleAnalyzerPlugin
-const TerserPlugin = require('terser-webpack-plugin')
+  .BundleAnalyzerPlugin
 
 module.exports = {
-    mode: 'production',
+  mode: 'production',
 
-    entry: {
-        x: './src/entry_x.js',
-        y: './src/entry_y.js',
-    },
-    output: {
-        filename: '[name]-bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    optimization: {
-        minimizer: [new TerserPlugin()],
-
-        splitChunks: {
-            maxInitialRequests: 30,
-            maxAsyncRequests: 30,
-            chunks: 'all',
-            cacheGroups: {
-                vendors: {
-                    test: /\/node_modules\/.+\.(js|es6)$/,
-                    priority: -10,
-                },
-                default: {
-                    test: /\.(js|es6)$/,
-                    minChunks: 1,
-                    priority: -20,
-                    reuseExistingChunk: true,
-                },
-            },
+  entry: {
+    x: './src/entry_x.js',
+    y: './src/entry_y.js',
+  },
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    splitChunks: {
+      maxInitialRequests: 30,
+      maxAsyncRequests: 30,
+      minSize: 0,
+      minChunks: 1,
+      chunks: 'all',
+      cacheGroups: {
+        vendors: {
+          test: /\/node_modules\/.+\.(js|es6)$/,
+          minChunks: 1,
+          minSize: 0,
+          priority: -10,
         },
+        default: {
+          test: /\.(js|es6)$/,
+          minChunks: 1,
+          minSize: 300,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
-    plugins: [
-        //new BundleAnalyzerPlugin()
-    ],
+  },
+  plugins: [new BundleAnalyzerPlugin()],
 }
